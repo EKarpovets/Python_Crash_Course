4dd4113577133cf5432374674d67a3b9bc6c7b2e
EKarpovets <ginger1992@list.ru>
2016-06-12 19:31:48 +0300

    Test commit

---
+++ Chapter_4/Exercise_4-1.py
@@ -0,0 +1,8 @@
+pizzas = ["pepperoni", "ham and mushrooms", "chicken barbecue"]
+for pizza in pizzas:
+    print(pizza)
+
+for pizza in pizzas:
+    print("I like " + pizza + " pizza.")
+
+print("I really love pizza!")
