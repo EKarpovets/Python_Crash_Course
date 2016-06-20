0d176b04bfccced50b018ba96f58bd9fab399a2f
EKarpovets <ginger1992@list.ru>
2016-06-20 22:55:27 +0300

    Chapter 5

---
+++ Chapter_5/Exercise_5-1.py
@@ -0,0 +1,6 @@
+bear = 'Medvedenka'
+print("Is bear == 'Medvedenka'? I predict True.")
+print(bear == 'Medvedenka')
+
+print("Is bear == 'Belyash'? I predict False.")
+print(bear == 'Belyash')
---
+++ Chapter_5/Exercise_5-2.py
@@ -0,0 +1,19 @@
+bear = "Medvedenka"
+print(bear.lower() == 'medvedenka')
+print(bear == 'medvedenka')
+
+age1 = 23
+age2 = 24
+print(age1 != age2)
+print(age1 == age2)
+print(age1 >= age2)
+print(age1 <= age2)
+print(age1 > age2)
+print(age1 < age2)
+print(age1 == 23 or age2 == 23)
+print(age1 == 23 and age2 == 23)
+
+bears = ['Medvedenka', 'Belyash', 'Mishutka']
+print("Belyash" in bears)
+print("Belyash" not in bears)
+
